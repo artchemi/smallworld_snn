354724f137af9f986288a1547a26912d164c99a0
BorisGrechkin <yoshimitsu878@gmail.com>
2024-06-05 13:54:01 +0300

    fix

--- notebooks/snn.py
+++ notebooks/snn.py
@@ -15,8 +15,8 @@ import networkx as nx
 
 
 n_input = 28 * 28  # input layer
-n_e = 100  # e - excitatory
-n_i = n_e  # i - inhibitory
+n_e = 200  # e - excitatory
+n_i = 2  # i - inhibitory
 
 v_rest_e = -60. * mV  # v - membrane potential
 v_reset_e = -65. * mV
@@ -41,13 +41,9 @@ stdp = '''w : 1
     dApost/dt = -Apost / taupost : 1 (event-driven)'''
 pre = '''ge += w
     Apre += dApre
-    w = clip(w + lr*Apost, 0, gmax)'''
+    w = clip(w + lr*Apost, 0, 1)'''
 post = '''Apost += dApost
-    w = clip(w + lr*Apre, 0, gmax)'''
-stdp_small = '''w : 1
-    lr : 1 (shared)
-    dApre/dt = -Apre / taupre : 1 (event-driven)
-    dApost/dt = -Apost / taupost : 1 (event-driven)'''
+    w = clip(w + lr*Apre, 0, 1)'''
 
 
 def generate_small_world(n_nodes, k, p):
@@ -96,16 +92,16 @@ class Model:
         # poisson generators one-to-all excitatory neurons with plastic connections
         app['S1'] = Synapses(app['PG'], app['EG'], stdp, on_pre=pre, on_post=post, method='euler', name='S1')
         app['S1'].connect()
-        app['S1'].w = 'rand()*gmax'  # random weights initialisation
+        app['S1'].w = 'rand() * 0.3'  # random weights initialisation
         app['S1'].lr = 1  # enable stdp
 
         # здесь надо добавить другое правило обучения, а не STDP
-        app['S_small_world'] = Synapses(app['EG'], app['EG'], stdp_small, on_pre=pre, on_post=post, method='euler', name='S_small_world')
+        app['S_small_world'] = Synapses(app['EG'], app['EG'], stdp, on_pre=pre, on_post=post, method='euler', name='S_small_world')
 
         for (u, v) in G.edges():
             # print(f'{u} --- {v}')
             app['S_small_world'].connect(j=u, i=v)
-            app['S_small_world'].w = 'rand()*gmax*0.1'  # здесь нужно сделать так, чтобы генерировались
+            app['S_small_world'].w = 'rand() * 0.3'  # здесь нужно сделать так, чтобы генерировались
             # рандомные веса на порядок меньше, чем основные
 
         if (debug):
@@ -114,9 +110,14 @@ class Model:
 
             # excitation neurons one-to-one inhibitory neurons
         app['S2'] = Synapses(app['EG'], app['IG'], 'w : 1', on_pre='ge += w', name='S2')
+        # app['S2'].connect(j=0, i=range(100))
+        # app['S2'].connect(j=1, i=range(100, 200))
+        # app['S2'].delay = 'rand()*10*ms'
+        # app['S2'].w = 'rand() * 0.3'  # random weights initialisation
+        # app['S2'].lr = 1
         app['S2'].connect(j='i')
         app['S2'].delay = 'rand()*10*ms'
-        app['S2'].w = 2
+        app['S2'].w = 3
         # very strong fixed weights to ensure corresponding inhibitory neuron will always fire
 
         # inhibitory neurons one-to-all-except-one excitatory neurons
@@ -132,54 +133,84 @@ class Model:
     def __getitem__(self, key):
         return self.net[key]
 
-    def train(self, X, num_of_examples, epoch=1):
+    def train(self, X, X_test, num_of_examples=None, epoch=1):
         self.net['S1'].lr = 1
+        self.net['S2'].lr = 1
         self.net['S_small_world'].lr = 1
         # stdp on
 
-        features = [[] for _ in range(len(X))]
+        features_train = [[] for _ in range(len(X))]
         start, stop = 0, 5000
         for ep in tqdm(range(epoch)):
             # data = X[num_of_examples * ep:num_of_examples * (ep + 1)]
             for idx in range(len(X)):
                 # active mode
-                inp = SpikeMonitor(self.net['PG'], name='PM')
-                mon = SpikeMonitor(self.net['EG'], name='RM')
-                mon2 = SpikeMonitor(self.net['IG'], name='IM')
-                wei = StateMonitor(self.net['S1'], ['w', 'Apre', 'Apost'], record=self.net['S1'][380, :5], name='S1MM')
+                #inp = SpikeMonitor(self.net['PG'], name='PM')
+                #mon = SpikeMonitor(self.net['EG'], name='RM')
+                #mon2 = SpikeMonitor(self.net['IG'], name='IM')
+                #wei = StateMonitor(self.net['S1'], ['w', 'Apre', 'Apost'], record=self.net['S1'][380, :5], name='S1MM')
 
-                self.net.add(inp)
-                self.net.add(mon)
-                self.net.add(mon2)
-                self.net.add(wei)
+                #self.net.add(inp)
+                #self.net.add(mon)
+                #self.net.add(mon2)
+                #self.net.add(wei)
                 
                 self.net['PG'].rates = X[idx].ravel() * Hz
                 self.net.run(0.35 * second)
 
-                features[idx].append(np.array(mon.count, dtype=int8))
+                #features_train[idx].append(np.array(mon.count, dtype=int8))
                 # passive mode
                 self.net['PG'].rates = np.zeros(n_input) * Hz
                 self.net.run(0.15 * second)
 
-                plot_w(wei, f'weight/weight_{ep}_{idx}.png')
-                plot_spikes(mon, mon2, f'spikes/spikes_{ep}_{idx}.png')
-                plot_spikes(inp, mon, f'spikes/input_{ep}_{idx}.png')
+                #if idx % num_of_examples == 0 and (ep > 25 or ep < 3): # сохранять только каждую num_of_examples итерацию
+                    #plot_w(wei, f'weight/weight_{ep}_{idx}.png')
+                    #plot_spikes(mon, mon2, f'spikes/spikes_{ep}_{idx}.png')
+                    #plot_spikes(inp, mon, f'spikes/input_{ep}_{idx}.png')
 
-                self.net.remove(self.net['PM'])
-                self.net.remove(self.net['RM'])
-                self.net.remove(self.net['IM'])
-                self.net.remove(self.net['S1MM'])
+                #self.net.remove(self.net['PM'])
+                #self.net.remove(self.net['RM'])
+                #self.net.remove(self.net['IM'])
+                #self.net.remove(self.net['S1MM'])
 
-                visualizing.plot_2d_heatmap(array=(np.asarray(self.net['ESM'].v / b2.mV).T)[start:stop, :], title='Membrane Potentials',
-                                            xlabel='Simulating Step',
-                                            ylabel='Neuron Index', int_x_ticks=True, x_max=5000, dpi=200)
+                '''if idx % num_of_examples == 0:
+                    visualizing.plot_2d_heatmap(array=(np.asarray(self.net['ESM'].v / b2.mV).T)[start:stop, :], title='Membrane Potentials',
+                                                xlabel='Simulating Step',
+                                                ylabel='Neuron Index', int_x_ticks=True, x_max=5000, dpi=200)
 
-                plt.savefig(f'spikes/heat_{ep}_{idx}.png')
-                plt.close()
-                start += 5000
-                stop += 5000
+                    plt.savefig(f'spikes/heat_{ep}_{idx}.png')
+                    plt.close()
+                    start += 5000
+                    stop += 5000'''
 
-        return features
+        self.net['S1'].lr = 0
+        self.net['S2'].lr = 0
+        self.net['S_small_world'].lr = 0
+
+        features_test = []
+        for idx in tqdm(range(len(X_test))):
+            mon_test = SpikeMonitor(self.net['EG'], name='TestRM')
+            mon2_test = SpikeMonitor(self.net['IG'], name='TestIM')
+
+            self.net.add(mon_test)
+            self.net.add(mon2_test)
+
+            # active mode
+            self.net['PG'].rates = X[idx].ravel() * Hz
+            self.net.run(0.35 * second)
+
+            # passive mode
+            self.net['PG'].rates = np.zeros(n_input) * Hz
+            self.net.run(0.15 * second)
+
+            plot_spikes(mon_test, mon2_test, f'spikes/test_{idx}.png')
+            # spikes per neuron foreach image
+            features_test.append(np.array(mon2_test.count))
+
+            self.net.remove(self.net['TestRM'])
+            self.net.remove(self.net['TestIM'])
+
+        return features_train, features_test
 
     def evaluate(self, X):
         self.net['S1'].lr = 0  # stdp off
@@ -267,15 +298,35 @@ def test0(train_items=30):
     plot_rates(model['ERM'], model['IRM'])
     plot_spikes(model['ESP'], model['ISP'])
 
+def create_dataset(X_train, y_train, labels, num_of_ex):
+
+    selected_matrices = []
+    selected_labels = []
+    selected_count = {label: 0 for label in set(y_train)}
+
+    for matrix, label in zip(X_train, y_train):
+        if selected_count[label] < num_of_ex and label in labels:
+            selected_matrices.append(matrix)
+            selected_labels.append(label)
+            selected_count[label] += 1
+
+    return np.array(selected_matrices), np.array(selected_labels)
+
 
 def main():
     (X_train, y_train), (X_test, y_test) = mnist.load_data()
 
-    # simplified classification (0 1 and 8)
-    # X_train = X_train[(y_train == 1) | (y_train == 0) | (y_train == 8)]
-    # y_train = y_train[(y_train == 1) | (y_train == 0) | (y_train == 8)]
-    # X_test = X_test[(y_test == 1) | (y_test == 0) | (y_test == 8)]
-    # y_test = y_test[(y_test == 1) | (y_test == 0) | (y_test == 8)]
+    # simplified classification (0 and 1)
+    X_train = X_train[(y_train == 1) | (y_train == 0)]
+    y_train = y_train[(y_train == 1) | (y_train == 0)]
+    X_test = X_test[(y_test == 1) | (y_test == 0)]
+    y_test = y_test[(y_test == 1) | (y_test == 0)]
+
+    # pixel intensity to Hz (255 becoms ~63Hz)
+    X_train = X_train / 4
+    X_test = X_test / 4
+
+    X_train.shape, X_test.shape
 
     # pixel intensity to Hz (255 becoms ~63Hz)
     X_train = X_train / 4
@@ -285,28 +336,31 @@ def main():
     selected_labels = []
     selected_count = {label: 0 for label in set(y_train)}
 
-    for matrix, label in sorted(zip(X_train, y_train), key=lambda x: x[1]):
-        if selected_count[label] < 1:
+    for matrix, label in zip(X_train, y_train):
+        if selected_count[label] < 10 and label in [7, 4]:
             selected_matrices.append(matrix)
             selected_labels.append(label)
             selected_count[label] += 1
 
     # Преобразуем списки в numpy массивы
-    selected_matrices = np.array(selected_matrices)
-    selected_labels = np.array(selected_labels)
+    X_train, y_train = create_dataset(X_train, y_train, [0, 1], 20)
+    X_test, y_test = create_dataset(X_test, y_test, [0, 1], 3)
 
     model = Model(True)
     assign_items = len(X_test)  # 60k
 
     seed(0)
 
-    f_test = model.train(selected_matrices[:2], 1, 30)
+    f_train, f_test = model.train(X_train, X_test, 20, epoch=20)
 
-    print(f_test)
+    for i, v in zip(f_test, y_test):
+        print(i, v)
+
+
     clf = RandomForestClassifier(max_depth=4, random_state=0)
-    clf.fit(f_test[-1], y_test)
+    clf.fit(f_train, y_train)
 
-    print(clf.score(f_test[-1], y_test))
+    print(clf.score(f_test, y_test))
 
     y_pred = clf.predict(f_test)
     conf_m = confusion_matrix(y_pred, y_test)
@@ -318,6 +372,7 @@ def main():
     # time - время
     # exc_rate, inh_rate - частота спайкования возбуждающих и ингибирующих нейронов
 
+    
     data_dict = {'time': model['ERM'].t / b2.ms,
                  'exc_rate': model['ERM'].smooth_rate(window='flat', width=0.1 * b2.ms) / b2.Hz,
                  'inh_rate': model['IRM'].smooth_rate(window='flat', width=0.1 * b2.ms) / b2.Hz}
@@ -350,7 +405,7 @@ def main():
     plt.show()
     plt.close()
 
-    # utils.plot_rates(model['ERM'], model['IRM'])
+    # utils.plot_rates(model['ERM'], model['IRM'])'''
 
     # model.net.restore()
 
